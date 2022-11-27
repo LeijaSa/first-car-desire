a714c5079b4525cd64ac2ac771d303cfa4b01ed1
Leija Salmivirta <leija.salmivirta@gmail.com>
2022-11-27 10:51:24 +0200

    Add react-router-dom, components Carlist and Login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { Container, Typography } from "@mui/material";
+//import Login from "./components/Login";
+//import Carlist from "./components/Carlist";
+import { Route, Routes } from "react-router-dom";
 
-function App() {
-   return (
-      <div className="App">
-         <header className="App-header"></header>
-      </div>
+const App: React.FC = (): React.ReactElement => {
+   const [token, setToken] = useState<string>(
+      String(localStorage.getItem("token"))
    );
-}
+
+   return (
+      <Container>
+         <Typography variant="h4" sx={{ marginBottom: 2, marginTop: 2 }}>
+            Autolistaus
+         </Typography>
+      </Container>
+   );
+};
 
 export default App;
