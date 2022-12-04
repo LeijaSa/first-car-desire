9d199f6a3d8cbf9906c7ab255aed95c8898780d2
Leija Salmivirta <leija.salmivirta@gmail.com>
2022-12-04 11:47:06 +0200

    Add App.test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import { Container, Typography } from "@mui/material";
 import Login from "./components/Login";
 import Carlist from "./components/Carlist";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App: React.FC = (): React.ReactElement => {
    const [token, setToken] = useState<string>(
@@ -11,15 +11,17 @@ const App: React.FC = (): React.ReactElement => {
    );
 
    return (
-      <Container>
-         <Typography variant="h4" sx={{ marginBottom: 2, marginTop: 2 }}>
-            Autolistaus
-         </Typography>
-         <Routes>
-            <Route path="/login" element={<Login setToken={setToken} />} />
-            <Route path="/" element={<Carlist token={token} />} />
-         </Routes>
-      </Container>
+      <Router>
+         <Container>
+            <Typography variant="h4" sx={{ marginBottom: 2, marginTop: 2 }}>
+               Autolistaus
+            </Typography>
+            <Routes>
+               <Route path="/login" element={<Login setToken={setToken} />} />
+               <Route path="/" element={<Carlist token={token} />} />
+            </Routes>
+         </Container>
+      </Router>
    );
 };
 
