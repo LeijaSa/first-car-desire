123422e1fb4d327b45ea14f74e9ba3af3ec50c84
Leija Salmivirta <leija.salmivirta@gmail.com>
2022-12-04 18:27:08 +0200

    Add react-renderer to dependencies

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
-test("should render without crashing", () => {
-   render(<App />);
+describe("<App />", () => {
+   test("should render without crashing", () => {
+      render(<App />);
+   });
 });
